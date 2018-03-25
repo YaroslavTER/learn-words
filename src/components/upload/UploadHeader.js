397c3e831a7eb7a94b6d76c541cbda7d52ba47d0
Yaroslav <yaroslavtereshchuk@gmail.com>
2018-03-25 19:02:20 +0300

    Updates

--- src/components/upload/UploadHeader.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-import "../../styles/App.css";
-
-export class UploadHeader extends Component {
-  render() {
-    return <h3 className="mt-3">{this.props.value}</h3>;
-  }
-}
